ceced1df6b3e502a6cc69da3559cf2466056b960
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2020-06-29 23:46:22 +0700

    Showing Image Page with Variative Title

---
+++ resources/views/staff_side/see-img.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" style="height: 100%;">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}}</title>
+</head>
+<body style="text-align: center; margin: 0; height: 100%;">
+    <img src="http://localhost:8000/photos/{{$table}}_id={{$id}}&opt={{$column}}" height="100%" style="">
+</body>
+</html>
