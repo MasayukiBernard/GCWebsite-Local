5d2567df0dcc8629525b68f5c177daf6f365cf98
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2020-07-17 14:04:57 +0700

    Account Email Verification

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,22 +3,21 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 text-center">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header h4">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
-                    @if (session('resent'))
+                    @if (session('sent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('A verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    Before you can proceed any further, please verify your email address by clicking the below link.
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">Send Email Verification Link</button>.
                     </form>
                 </div>
             </div>
