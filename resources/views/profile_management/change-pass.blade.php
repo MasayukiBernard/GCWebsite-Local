7205b6625d095f4fe036b7b0127868df0abc47e3
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2020-08-03 21:17:53 +0700

    Update view

--- resources/views/profile_management/change-pass.blade.php
+++ resources/views/profile_management/change-pass.blade.php
@@ -5,22 +5,42 @@
 @endsection
 
 @section('content')
-    This is change password page!<br>
-    Password cannot contain any white-space character or symbol.<br>
-    Password must have a length of at least 8 characters.<br>
-    Password can contain special characters.<br><br>
-    <form method="POST" action="{{ route('change-pass')}}">
-        @csrf
-        <label>New Password</label><br>
-        <input type="password" class="@error('new-pass') is-invalid @enderror" name="new-pass"><br>
-        @error('new-pass')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <label>Confirm New Password</label><br>
-        <input type="password" class="@error('confirm-new-pass') is-invalid @enderror"name="confirm-new-pass"><br>
-        @error('confirm-new-pass')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <input type="submit" value="Change Password">
-    </form>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header h3">
+                    Change Profile Password
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6 border-right">
+                            <form method="POST" action="{{ route('change-pass')}}">
+                                @csrf
+                                <label>New Password</label><br>
+                                <input type="password" class="form-control @error('new-pass') is-invalid @enderror" name="new-pass"><br>
+                                @error('new-pass')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                                <label>Confirm New Password</label><br>
+                                <input type="password" class="form-control @error('confirm-new-pass') is-invalid @enderror"name="confirm-new-pass"><br>
+                                @error('confirm-new-pass')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                                <input class="btn btn-primary" type="submit" value="Change Password">
+                            </form>
+                        </div>
+                        <div class="col-md-6">
+                            <h5><b>Password making rules:</b></h5>
+                            <ul>
+                                <li>Password cannot contain any white-space character or symbol.<br></li>
+                                <li>Password must have a length of at least 8 characters.<br></li>
+                                <li>Password can contain special characters.<br><br></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
